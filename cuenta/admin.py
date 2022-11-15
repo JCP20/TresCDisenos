6f493b0f972d335e076b7d2977a9edd64be01862
Juliana Cardozo <cardozojuliana908@gmail.com>
2022-11-14 21:50:22 -0500

    Creacion de bases artículos

--- cuenta/admin.py
+++ cuenta/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
-
+from .models import Coleccion,Artículo
 # Register your models here.
+admin.site.register(Coleccion)
+admin.site.register(Artículo)
--- cuenta/models.py
+++ cuenta/models.py
@@ -4,12 +4,11 @@ from django.db import models
 class Coleccion(models.Model):
     nombre = models.CharField(max_length=200)
     vigencia_coleccion = models.CharField(max_length=200)
-    tipos_prenda_col = models.CharField(max_length=50)
+    tipo_de_prenda = models.CharField(max_length=50)
 class Artículo(models.Model):
     nombre = models.CharField(max_length=200)
     tipo_articulo = models.CharField(max_length=200)
     material = models.CharField(max_length=50)
-    coleccion = models.ForeignKey(Coleccion, on_delete=models.DO_NOTHING)
     color = models.CharField(max_length=20)
     descripcion = models.TextField()
     precio = models.IntegerField()
