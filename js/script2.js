e4e7e89feb093f8086127fab7529c9db58258362
Laura Guacaneme <62676914+Guacaneme@users.noreply.github.com>
2022-11-16 11:52:23 -0500

    Documentación script2.js

--- js/script2.js
+++ js/script2.js
@@ -1,23 +1,22 @@
 $(document).ready(function(){
-
-    $('.items_options .i_option[category="p_oferta"]').addClass('ct_item-active');
-    $('.item').hide();
-    $('.item[category="p_oferta"]').show();
-    $('.i_option').click(function(){
+    $('.items_options .i_option[category="p_oferta"]').addClass('ct_item-active');  // Se agrega la clase para cambiar el estilo a la categoría que se encuentra seleccionada por defecto
+    $('.item').hide();                                 // Se ocultan los artículos 
+    $('.item[category="p_oferta"]').show();            // Se muestran los artículos que corresponden a la categoría por defecto
+    $('.i_option').click(function(){                   // Función que se implementa al oprimir cualquiera de los 3 botones para selección de categoría 
         var categoria = $(this).attr('category');
 
-        $('.i_option').removeClass('ct_item-active');
-        $(this).addClass('ct_item-active');
+        $('.i_option').removeClass('ct_item-active');  // Se remueve la clase que cambia el estilo a todos los botones 
+        $(this).addClass('ct_item-active');            // Se aplica el estilo a el botón de la categoría seleccionado
 
-        $('.item').css('transform', 'scale(0)');
+        $('.item').css('transform', 'scale(0)');       // Se aplica una transformación al objeto artículo para aplicar el efecto
         function ocultarItems() {
             $('.item').hide();
-        } setTimeout(ocultarItems,400);
+        } setTimeout(ocultarItems,400);                // Se aplica un temporizador para que se note el efecto y después ocultar todos los artículos 
 
         function mostrarItems() {
             $('.item[category="'+categoria+'"]').show();
             $('.item[category="'+categoria+'"]').css('transform', 'scale(1)');            
-        } setTimeout(mostrarItems,400);
+        } setTimeout(mostrarItems,400);                // De igual forma se usa el temporizador para mostrar los artículos filtrados por categoría 
 
     });
 })
