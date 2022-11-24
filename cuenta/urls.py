5ec7e753ade7dafb051e75e0200bbb8aa14cdd87
Juliana Cardozo <cardozojuliana908@gmail.com>
2022-11-24 17:25:20 -0500

    organización

--- cuenta/urls.py
+++ cuenta/urls.py
@@ -1,5 +1,9 @@
 from django.urls import path
 from . import views
 urlpatterns = [
-    path('', views.mosaico)
+    path('', views.galeria),
+    path('galeria', views.galeria),
+    path('tienda', views.tienda),
+    path('login', views.login),
+    path('registro', views.registro)
 ]
--- cuenta/views.py
+++ cuenta/views.py
@@ -2,5 +2,17 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 # Create your views here.
-def mosaico(request):
-    return render(request,'mosaico.html')
+def galeria(request):
+    return render(request,'galeria.html')
+def cuenta(request):
+    return render(request,'cuenta.html')
+def infocuenta(request):
+    return render(request,'detalles.html')
+def bolsa(request):
+    return render(request,'bolsa.html')
+def login(request):
+    return render(request,'Login.html')
+def registro(request):
+    return render(request,'Registro.html')
+def tienda(request):
+    return render(request,'tienda.html')
