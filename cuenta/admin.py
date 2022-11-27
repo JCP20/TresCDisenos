238c4b3d4325f9559faf8f6f785dd69000ef7860
Juliana Cardozo <cardozojuliana908@gmail.com>
2022-11-27 16:41:11 -0500

    modelos Cambios

--- cuenta/admin.py
+++ cuenta/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import Coleccion,Artículo
+from .models import Coleccion,Artículo,CarroCompra
 # Register your models here.
 admin.site.register(Coleccion)
 admin.site.register(Artículo)
+admin.site.register(CarroCompra)
--- cuenta/models.py
+++ cuenta/models.py
@@ -1,6 +1,7 @@
 from django.db import models
-
-
+from django.conf import settings
+class AlbumFoto(models.Model):
+    fotos = models.ImageField()
 # Create your models here.
 class Coleccion(models.Model):
     nombre = models.CharField(max_length=200)
@@ -16,8 +17,14 @@ class Artículo(models.Model):
     descripcion = models.TextField()
     precio = models.IntegerField()
     fotos = models.ImageField()
-    talla = models.CharField(max_length=20)
-    cant_disponible_por_talla = models.IntegerField()
+    cant_disponible_XS= models.IntegerField()
+    cant_disponible_S= models.IntegerField()
+    cant_disponible_M= models.IntegerField()
+    cant_disponible_L= models.IntegerField()
+    cant_disponible_XL= models.IntegerField()
     coleccion= models.ForeignKey(Coleccion, on_delete=models.SET_NULL, null=True)
     def __str__(self) -> str:
         return self.nombre
+class CarroCompra(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete= models.CASCADE)
+    items = models.ManyToManyField(Artículo)
