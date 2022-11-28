84aff458759f628fab5199bcec60a4fc14084ebc
Juliana Cardozo <cardozojuliana908@gmail.com>
2022-11-27 19:33:14 -0500

    cuenta

--- cuenta/urls.py
+++ cuenta/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 from . import views
 urlpatterns = [
-    path('login/', views.bolsa),
+    path('login/cuenta', views.cuenta),
     path('', views.galeria),
     path('galeria', views.galeria),
     path('tienda', views.tienda),
     path('login', views.login),
     path('registro', views.registro),
+    path('login/cuenta/', views.bolsa)
 ]
